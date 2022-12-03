5ab5de1f1fadc5d29e7b3a38abce6551c80e9bed
Henry Tu <hungtp2912@gmail.com>
2022-12-03 17:24:18 -0600

    Add the server side code

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "es2021": true
+    },
+    "extends": "plugin:vue/vue3-essential",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
