20e65e9028614e9bd2c2acc28e457b33e5732af8
Henry Tu <hungtp2912@gmail.com>
2022-12-03 18:36:59 -0600

    Creating Register page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" placeholder="email" v-model="email"/>
+    <br/>
+    <input type="password" name="password" placeholder="password" v-model="password"/>
+    <br/>
+    <button type="submit">Register</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('Email has changed!', value)
+    }
+  },
+  mounted () {
+    setTimeout(function () {
+      this.email = 'hello world'
+    }, 2000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
+
+<script>
+export default {
+  data(){
+    return{
+      msg: 'Hello'
+    }
+  }
+}
+</script>
