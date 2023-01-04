7de142c1a9c136cdee94c6e503e24dce2732fb2c
Henry Tu <hungtp2912@gmail.com>
2023-01-03 22:22:00 -0600

    Redo the project

--- server/src/models/User.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) =>
-    sequelize.define('User', {
-        email: {
-            type: DataTypes.STRING,
-            unique: true //Set no duplicate email
-        },
-        password: DataTypes.STRING
-    })
